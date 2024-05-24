7ab42ee5774cc6be8562165a1e703dba1292647e
Hitkumar <hit98987@gmail.com>
2024-05-24 17:07:12 -0400

    initial commit

---
+++ components/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: "1",
+        type : "left",
+        message : "hii"
+    },
+    {
+        id: "2",
+        type : "right",
+        message : "hello"
+    },
+    {
+        id: "3",
+        type : "left",
+        message : "how are you"
+    },
+    {
+        id: "4",
+        type : "right",
+        message : "i am fine"
+    },
+]
+
+export default data
